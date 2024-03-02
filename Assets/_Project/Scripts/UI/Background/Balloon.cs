9b451260b86837cb94d4d1b0c787de3c19621755
TheOctan <kanatush.tim@gmail.com>
2024-03-02 20:20:55 +0300

    Sinus wave trajectory

--- Assets/_Project/Scripts/UI/Background/Balloon.cs
+++ Assets/_Project/Scripts/UI/Background/Balloon.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,21 +11,34 @@ namespace OctanGames.UI.Background
             Right
         }
 
-        [SerializeField] private float _speed;
-        [SerializeField] private float _amplitude;
-        [SerializeField] private float _heightOffset;
-
         [SerializeField] private SpriteRenderer _spriteRenderer;
 
         private Bounds _bounds;
         private Vector3 _spriteBoundsExtents;
+        private Vector3 _startPosition;
+
+        private Vector2 _horizontalSpeedRange;
+        private Vector2 _verticalSpeedRange;
+        private Vector2 _amplitudeRange;
+        
+        private float _horizontalSpeed;
+        private float _verticalSpeed;
+        private float _amplitude;
+
         private Side _destinationSide;
         private float _leftBorder;
         private float _rightBorder;
 
-        public void Setup(Bounds bounds)
+        public void Setup(Bounds bounds,
+            Vector2 horizontalSpeedRange,
+            Vector2 verticalSpeedRange,
+            Vector2 amplitudeRange)
         {
             _bounds = bounds;
+            _amplitudeRange = amplitudeRange;
+            _verticalSpeedRange = verticalSpeedRange;
+            _horizontalSpeedRange = horizontalSpeedRange;
+
             _spriteBoundsExtents = _spriteRenderer.bounds.extents;
             _leftBorder = _bounds.min.x - _spriteBoundsExtents.x;
             _rightBorder = _bounds.max.x + _spriteBoundsExtents.x;
@@ -36,27 +48,30 @@ namespace OctanGames.UI.Background
 
         private void Update()
         {
-            Vector3 deltaPosition = Time.deltaTime * _speed * Vector3.right;
+            float deltaPosition = Time.deltaTime * _horizontalSpeed;
+            float yPosition = Mathf.Sin(Time.time * _verticalSpeed) * _amplitude;
 
             switch (_destinationSide)
             {
                 case Side.Left:
-                    transform.position -= deltaPosition;
+                    transform.position = new Vector3(transform.position.x - deltaPosition,
+                        _startPosition.y + yPosition, 0);
+
                     if (transform.position.x <= _leftBorder)
                     {
                         GenerateTrajectory();
                     }
                     break;
                 case Side.Right:
-                    transform.position += deltaPosition;
+                    transform.position = new Vector3(transform.position.x + deltaPosition,
+                        _startPosition.y + yPosition, 0);
+
                     if (transform.position.x >= _rightBorder)
                     {
                         GenerateTrajectory();
                     }
                     break;
             }
-
-            //float y = Mathf.Sin(Time.time * _speed) * _amplitude;
         }
 
         private void GenerateTrajectory()
@@ -66,9 +81,14 @@ namespace OctanGames.UI.Background
                 _bounds.max.y - _spriteBoundsExtents.y);
 
             _destinationSide = GetRandomSide();
-
             float randomXPosition = _destinationSide == Side.Left ? _rightBorder : _leftBorder;
-            transform.position = new Vector3(randomXPosition, randomYPosition, 0);
+
+            _startPosition = new Vector3(randomXPosition, randomYPosition, 0);
+            transform.position = _startPosition;
+
+            _horizontalSpeed = Random.Range(_horizontalSpeedRange.x, _horizontalSpeedRange.y);
+            _verticalSpeed = Random.Range(_verticalSpeedRange.x, _verticalSpeedRange.y);
+            _amplitude = Random.Range(_amplitudeRange.x, _amplitudeRange.y);
         }
 
         private static Side GetRandomSide()
--- Assets/_Project/Scripts/UI/Background/BalloonSpawner.cs
+++ Assets/_Project/Scripts/UI/Background/BalloonSpawner.cs
@@ -9,12 +9,15 @@ namespace OctanGames.UI.Background
     {
         [Header("Properties")]
         [SerializeField, Min(0)] private int _maxBalloons = 3;
+        [Space]
+        [SerializeField] private Vector2 _horizontalSpeedRange;
+        [SerializeField] private Vector2 _verticalSpeedRange;
+        [SerializeField] private Vector2 _amplitudeRange;
         [Header("Resources")]
         [SerializeField] private Camera _mainCamera;
         [SerializeField] private List<Balloon> _balloonPrefabs;
 
         private List<Balloon> _spawnedBalloons = new();
-        
 
         private void Start()
         {
@@ -36,7 +39,7 @@ namespace OctanGames.UI.Background
                 Balloon balloon = Instantiate(balloonPrefab, transform);
 
                 Bounds cameraBounds = _mainCamera.OrthographicBounds();
-                balloon.Setup(cameraBounds);
+                balloon.Setup(cameraBounds, _horizontalSpeedRange, _verticalSpeedRange, _amplitudeRange);
             }
         }
     }
