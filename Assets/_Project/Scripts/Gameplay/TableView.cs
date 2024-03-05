4b6c6a01f90a2a95ad6460424cbe6838f1aada9f
TheOctan <kanatush.tim@gmail.com>
2024-03-05 12:00:22 +0300

    Table view

---
+++ Assets/_Project/Scripts/Gameplay/TableView.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using Extensions;
+using OctanGames.Extensions;
+using OctanGames.Gameplay.Levels;
+using OctanGames.Infrastructure;
+using OctanGames.Inputs;
+using UnityEngine;
+
+namespace OctanGames.Gameplay
+{
+    public class TableView : MonoBehaviour
+    {
+        public event Action<Vector2Int, Vector2Int> CellMoved;
+
+        [SerializeField] private float _width;
+        [SerializeField] private float _height;
+
+        private int _rows;
+        private int _columns;
+        private CellView[,] _cellMap;
+        private bool _isAnimated;
+
+        private ILevelLibrary _levelLibrary;
+        private CellSettings _cellSettings;
+
+        private void Start()
+        {
+            _levelLibrary = ServiceLocator.GetInstance<ILevelLibrary>();
+            _cellSettings = ServiceLocator.GetInstance<CellSettings>();
+        }
+
+        private void SetCellSortingOrder(CellView cell, Vector2Int index)
+        {
+            cell.SetSortingOrder(index.y * _rows + (_rows - index.x));
+        }
+        private void OnCellSwiped(CellView cell, SwipeDirection swipeDirection)
+        {
+            if(_isAnimated)
+            {
+                return;
+            }
+
+            Vector2Int cellPosition = GetCellPosition(cell);
+            if (cellPosition == new Vector2Int(-1,-1))
+            {
+                return;
+            }
+
+            Vector2Int nextPosition = GetNextPosition(cellPosition, swipeDirection);
+            if (nextPosition == cellPosition)
+            {
+                return;
+            }
+
+            CellMoved?.Invoke(cellPosition, nextPosition);
+        }
+        private Vector2Int GetCellPosition(CellView cell)
+        {
+            for (var i = 0; i < _rows; i++)
+            {
+                for (var j = 0; j < _columns; j++)
+                {
+                    if (_cellMap[i, j] == cell)
+                    {
+                        return new Vector2Int(i, j);
+                    }
+                }
+            }
+
+            return new Vector2Int(-1, -1);
+        }
+        private Vector2Int GetNextPosition(Vector2Int currentPosition, SwipeDirection swipeDirection)
+        {
+            int x = currentPosition.x;
+            int y = currentPosition.y;
+
+            switch (swipeDirection)
+            {
+                case SwipeDirection.Up:
+                    if (x > 0 && _cellMap[x - 1, y] != null)
+                    {
+                        return new Vector2Int(x - 1, y);
+                    }
+                    break;
+                case SwipeDirection.Down:
+                    if (x < _rows - 1)
+                    {
+                        return new Vector2Int(x + 1, y);
+                    }
+                    break;
+                case SwipeDirection.Right:
+                    if (y < _columns - 1)
+                    {
+                        return new Vector2Int(x, y + 1);
+                    }
+                    break;
+                case SwipeDirection.Left:
+                    if (y > 0)
+                    {
+                        return new Vector2Int(x, y - 1);
+                    }
+                    break;
+            }
+
+            return currentPosition;
+        }
+    }
+}
--- Assets/_Project/Scripts/Infrastructure/GameBootstrapper.cs
+++ Assets/_Project/Scripts/Infrastructure/GameBootstrapper.cs
@@ -1,9 +1,20 @@
+using OctanGames.Gameplay;
+using OctanGames.Gameplay.Levels;
 using UnityEngine;
 
 namespace OctanGames.Infrastructure
 {
     public class GameBootstrapper : MonoBehaviour
     {
-        
+        [SerializeField] private CellSettings _cellSettings;
+        [SerializeField] private LevelLibraryLibrary _levels;
+        [SerializeField] private TableView _tableView;
+
+        private void Awake()
+        {
+            ServiceLocator.Bind(_cellSettings);
+            ServiceLocator.Bind<ILevelLibrary>(_levels);
+            ServiceLocator.Bind(_tableView);
+        }
     }
 }
