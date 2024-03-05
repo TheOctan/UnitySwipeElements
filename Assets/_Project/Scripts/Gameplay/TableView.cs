32e5842c4d4277ad4af0cb683d993a4824a83472
TheOctan <kanatush.tim@gmail.com>
2024-03-05 19:27:27 +0300

    Animate cell falling and destroying

--- Assets/_Project/Scripts/Gameplay/TableView.cs
+++ Assets/_Project/Scripts/Gameplay/TableView.cs
@@ -12,7 +12,7 @@ namespace OctanGames.Gameplay
 {
     public class TableView : MonoBehaviour
     {
-        private const int EMPTY_CELL = 0;
+        public const int EMPTY_CELL = 0;
 
         public event Action<Vector2Int, Vector2Int> CellMoved;
 
@@ -26,6 +26,8 @@ namespace OctanGames.Gameplay
 
         private CellSettings _cellSettings;
         private LevelLoader _levelLoader;
+        private GridController _gridController;
+
         private CellView[,] _cellMap;
         private Vector2 _cellSize;
         private Vector3 _finalLeftUpCorner;
@@ -42,7 +44,11 @@ namespace OctanGames.Gameplay
         {
             _cellSettings = ServiceLocator.GetInstance<CellSettings>();
             _levelLoader = ServiceLocator.GetInstance<LevelLoader>();
+            _gridController = new GridController(this);
+
             DOTween.defaultAutoPlay = AutoPlay.None;
+
+            InitNewLevel();
         }
         private void OnDrawGizmos()
         {
@@ -55,56 +61,125 @@ namespace OctanGames.Gameplay
                 corner.RightDownCorner);
         }
 
-        private Vector3 IndexToPosition(Vector2Int index)
-        {
-            var leftUpCellCorner = new Vector3(
-                _finalLeftUpCorner.x + _cellSize.x * index.y,
-                _finalLeftUpCorner.y - _cellSize.y * index.x);
-
-            return leftUpCellCorner.GetCenter(_cellSize);
-        }
-
-        public void CellPlaceChangedAnim(Vector2Int startPosition, Vector2Int endPosition)
+        public void AnimateCellSwapping(Vector2Int startPosition, Vector2Int endPosition)
         {
             var animationList = new List<Sequence>();
 
-            void UpdateCellPosition(CellView cell, Vector2Int targetIndex)
+            CellView firstCell = GetCellByIndex(startPosition);
+            Sequence sequence = GetCellMovementSequence(firstCell, endPosition, _movementDuration);
+            animationList.Add(sequence);
+
+            CellView secondCell = GetCellByIndex(endPosition);
+            if (secondCell != null)
             {
-                if (cell == null)
-                {
-                    return;
-                }
-
-                Vector3 newPosition = IndexToPosition(targetIndex);
-                Sequence sequence = DOTween.Sequence();
-
-                if (cell.transform.position.x < newPosition.x)
-                {
-                    sequence.AppendCallback(() => SetCellSortingOrder(cell, targetIndex));
-                }
-
-                sequence.Append(DOTween.To(() => cell.transform.position, cell.SetPosition, newPosition, _movementDuration));
-
-                if (cell.transform.position.x >= newPosition.x)
-                {
-                    sequence.AppendCallback(() => SetCellSortingOrder(cell, targetIndex));
-                }
+                sequence = GetCellMovementSequence(secondCell, startPosition, _movementDuration);
                 animationList.Add(sequence);
             }
-
-            CellView firstCell = _cellMap[startPosition.x, startPosition.y];
-            UpdateCellPosition(firstCell, endPosition);
-            
-            CellView secondCell = _cellMap[endPosition.x, endPosition.y];
-            UpdateCellPosition(secondCell, startPosition);
 
             if (animationList.Count > 0)
             {
                 AddAnimation(animationList);
             }
 
-            _cellMap[startPosition.x, startPosition.y] = secondCell;
-            _cellMap[endPosition.x, endPosition.y] = firstCell;
+            SetCellByIndex(startPosition, secondCell);
+            SetCellByIndex(endPosition, firstCell);
+        }
+        public void AnimateCellFalling(List<FallData> cellModelFalls)
+        {
+            var animList = new List<Sequence>();
+            foreach (FallData modelFall in cellModelFalls)
+            {
+                CellView cell = _cellMap[modelFall.StartIndex.x, modelFall.StartIndex.y];
+
+                Vector2Int targetIndex = modelFall.StartIndex;
+                targetIndex.x += modelFall.FallStep;
+
+                float duration = _fallDuration * modelFall.FallStep;
+                Sequence sequence = GetCellMovementSequence(cell, targetIndex, duration);
+                animList.Add(sequence);
+
+                SetCellByIndex(targetIndex, cell);
+                SetCellByIndex(modelFall.StartIndex, null);
+            }
+
+            if (animList.Count > 0)
+            {
+                AddAnimation(animList);
+            }
+        }
+        public void AnimateCellDestroying(List<Vector2Int> indexes)
+        {
+            var animList = new List<Sequence>();
+            foreach (Vector2Int index in indexes)
+            {
+                CellView cell = _cellMap[index.x, index.y];
+                animList.Add(DOTween.Sequence()
+                    .Append(DOTween.To(() => cell.DestroyAnimationFrame, x => cell.DestroyAnimationFrame = x, cell.BoomIndexMax,
+                        _destructionDuration)).SetEase(Ease.Linear)
+                    .AppendCallback(() => cell.Destroy()));
+
+                _cellMap[index.x, index.y] = null;
+            }
+
+            if (animList.Count > 0)
+            {
+                AddAnimation(animList);
+            }
+        }
+
+        public void SwitchNextLevel()
+        {
+            _levelLoader.SwitchNextLevel();
+            DestroyTable();
+            InitNewLevel();
+        }
+        public void SwitchNextLevelAfterAnimation()
+        {
+            _levelLoader.SwitchNextLevel();
+            _callBackAfterAnimation = InitNewLevel;
+        }
+        private void InitNewLevel()
+        {
+            int[,] map = _levelLoader.LoadCurrentLevel();
+            _rows = map.GetLength(0);
+            _columns = map.GetLength(1);
+
+            CornerTuple tableCorners = transform.position.GetCornersFromCenter(_tableHeight, _tableWidth);
+
+            GenerateTable(map, tableCorners);
+            _gridController.Init(new GridModel(map, _rows, _columns));
+            _callBackAfterAnimation = null;
+
+            Debug.Log("Init new level");
+        }
+        private void DestroyTable()
+        {
+            if (_cellMap == null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < _rows; i++)
+            {
+                for (var j = 0; j < _columns; j++)
+                {
+                    if (_cellMap[i, j] != null)
+                    {
+                        _cellMap[i, j].Destroy();
+                    }
+                }
+            }
+        }
+
+        private Sequence GetCellMovementSequence(CellView cell, Vector2Int targetIndex, float duration)
+        {
+            Vector3 newPosition = IndexToPosition(targetIndex);
+
+            Sequence sequence = DOTween.Sequence()
+                .AppendCallback(() => SetCellSortingOrder(cell, targetIndex))
+                .Append(DOTween.To(() => cell.transform.position, cell.SetPosition, newPosition, duration));
+
+            return sequence;
         }
         private void AddAnimation(List<Sequence> listAnimations)
         {
@@ -114,7 +189,7 @@ namespace OctanGames.Gameplay
             }
             _animationQueue.Enqueue(listAnimations);
 
-            if (_isAnimated == false)
+            if (!_isAnimated)
             {
                 PlayAnimations();
             }
@@ -142,6 +217,7 @@ namespace OctanGames.Gameplay
                 _callBackAfterAnimation?.Invoke();
             }
         }
+
         private void GenerateTable(int[,] map, CornerTuple tableCorners)
         {
             _cellMap = new CellView[_rows, _columns];
@@ -169,19 +245,19 @@ namespace OctanGames.Gameplay
                 for (var j = 0; j < _columns; j++)
                 {
                     var indexPosition = new Vector2Int(i, j);
-                    var leftUpCellPosition = new Vector3(
+                    var leftUpCellCorner = new Vector3(
                         _finalLeftUpCorner.x + _cellSize.x * j,
                         _finalLeftUpCorner.y - _cellSize.y * i);
 
-                    Vector3 cellPosition = leftUpCellPosition.GetCenter(_cellSize);
+                    Vector3 cellPosition = leftUpCellCorner.GetCenter(_cellSize);
 
                     SetupCell(map, indexPosition, cellPosition, _cellSize);
                 }
             }
         }
-        private void SetupCell(int[,] map, Vector2Int indexPosition, Vector3 cellPosition, Vector2 cellSize)
+        private void SetupCell(int[,] map, Vector2Int index, Vector3 cellPosition, Vector2 cellSize)
         {
-            int cellType = map[indexPosition.x, indexPosition.y];
+            int cellType = map[index.x, index.y];
             if (cellType == EMPTY_CELL)
             {
                 return;
@@ -190,14 +266,15 @@ namespace OctanGames.Gameplay
             CellView cell = Instantiate(_cellSettings.CellPrefab);
             CellSettings.CellAnimation cellAnimation = _cellSettings.CellAnimations[cellType - 1];
             cell.SetAnimation(cellAnimation);
-            SetCellSortingOrder(cell, indexPosition);
+            SetCellSortingOrder(cell, index);
             cell.SetSize(cellSize);
             cell.SetPosition(cellPosition);
             cell.Init();
             cell.Swiped += OnCellSwiped;
 
-            _cellMap[indexPosition.x, indexPosition.y] = cell;
+            _cellMap[index.x, index.y] = cell;
         }
+
         private void SetCellSortingOrder(CellView cell, Vector2Int index)
         {
             cell.SetSortingOrder(index.y * _rows + (_rows - index.x));
@@ -209,13 +286,13 @@ namespace OctanGames.Gameplay
                 return;
             }
 
-            Vector2Int cellPosition = GetCellPosition(cell);
+            Vector2Int cellPosition = GetCellIndex(cell);
             if (cellPosition == _invalidPosition)
             {
                 return;
             }
 
-            Vector2Int nextPosition = GetNextPosition(cellPosition, swipeDirection);
+            Vector2Int nextPosition = GetNextIndex(cellPosition, swipeDirection);
             if (nextPosition == cellPosition)
             {
                 return;
@@ -223,22 +300,7 @@ namespace OctanGames.Gameplay
 
             CellMoved?.Invoke(cellPosition, nextPosition);
         }
-        private Vector2Int GetCellPosition(CellView cell)
-        {
-            for (var i = 0; i < _rows; i++)
-            {
-                for (var j = 0; j < _columns; j++)
-                {
-                    if (_cellMap[i, j] == cell)
-                    {
-                        return new Vector2Int(i, j);
-                    }
-                }
-            }
-
-            return _invalidPosition;
-        }
-        private Vector2Int GetNextPosition(Vector2Int currentPosition, SwipeDirection swipeDirection)
+        private Vector2Int GetNextIndex(Vector2Int currentPosition, SwipeDirection swipeDirection)
         {
             int x = currentPosition.x;
             int y = currentPosition.y;
@@ -272,6 +334,38 @@ namespace OctanGames.Gameplay
             }
 
             return currentPosition;
+        }
+
+        private Vector3 IndexToPosition(Vector2Int index)
+        {
+            var leftUpCellCorner = new Vector3(
+                _finalLeftUpCorner.x + _cellSize.x * index.y,
+                _finalLeftUpCorner.y - _cellSize.y * index.x);
+
+            return leftUpCellCorner.GetCenter(_cellSize);
+        }
+        private Vector2Int GetCellIndex(CellView cell)
+        {
+            for (var i = 0; i < _rows; i++)
+            {
+                for (var j = 0; j < _columns; j++)
+                {
+                    if (_cellMap[i, j] == cell)
+                    {
+                        return new Vector2Int(i, j);
+                    }
+                }
+            }
+
+            return _invalidPosition;
+        }
+        private CellView GetCellByIndex(Vector2Int index)
+        {
+            return _cellMap[index.x, index.y];
+        }
+        private void SetCellByIndex(Vector2Int index, CellView cell)
+        {
+            _cellMap[index.x, index.y] = cell;
         }
     }
 }
