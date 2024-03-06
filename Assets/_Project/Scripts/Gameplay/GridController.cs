d2335b509d9f50b5436902bef02b2058dee6eea8
TheOctan <kanatush.tim@gmail.com>
2024-03-06 12:43:07 +0300

    Grid model and grid controller

---
+++ Assets/_Project/Scripts/Gameplay/GridController.cs
@@ -0,0 +1,25 @@
+﻿namespace OctanGames.Gameplay
+{
+    public class GridController
+    {
+        private readonly TableView _tableView;
+        private GridModel _gridModel;
+
+        public GridController(TableView tableView)
+        {
+            _tableView = tableView;
+        }
+
+        public void Init(GridModel model)
+        {
+            if (_gridModel != null)
+            {
+                _gridModel.LevelFinished -= _tableView.SwitchNextLevelAfterAnimation;
+            }
+
+            _gridModel = model;
+
+            _gridModel.LevelFinished += _tableView.SwitchNextLevelAfterAnimation;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Gameplay/GridModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OctanGames.Gameplay
+{
+    public class GridModel
+    {
+        public const int EMPTY_CELL = 0;
+        
+        public event Action LevelFinished;
+
+        private readonly int[,] _map;
+        private readonly int _rows;
+        private readonly int _columns;
+
+        public GridModel(int[,] map, int rows, int columns)
+        {
+            _map = map;
+            _rows = rows;
+            _columns = columns;
+        }
+
+        private bool IsLevelFinished()
+        {
+            for (var i = 0; i < _rows; i++)
+            {
+                for (var j = 0; j < _columns; j++)
+                {
+                    if (_map[i, j] != EMPTY_CELL)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            LevelFinished?.Invoke();
+            return true;
+        }
+    }
+}
